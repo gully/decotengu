c8d9672af8ffd35f5d2c758ff2a24bfaa118c83e
wrobell <wrobell@pld-linux.org>
2013-08-17 11:14:06 +0100

    - changed write_csv function to accept file object instead of a file name - implemented unit tests for above function - added documentation for above function

---
+++ decotengu/tests/test_util.py
@@ -0,0 +1,28 @@
+#
+# DecoTengu - dive decompression library.
+#
+# Copyright (C) 2013 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+DecoTengu various utilities tests.
+"""
+
+import unittest
+
+class WriteCSVTestCase(unittest.TestCase):
+
+# vim: sw=4:et:ai
