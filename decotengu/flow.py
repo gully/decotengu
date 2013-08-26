d6f52f8b1db279527e6842b38e3825c1fa56230d
wrobell <wrobell@pld-linux.org>
2013-08-26 20:42:25 +0100

    - basic support for coroutines added (copied from Kenozooid)

---
+++ decotengu/flow.py
@@ -0,0 +1,54 @@
+#
+# DecoTengu - dive decompression library.
+#
+# Copyright (C) 2013 by Artur Wroblewski <wrobell@pld-linux.org>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+"""
+DecoTengu data flow procesing functions and coroutines.
+"""
+
+
+def coroutine(func):
+    """
+    Decorator for a coroutine function.
+    
+    Advances a coroutine to its first ``(yield)`` statement.
+    """
+    def start(*args, **kwargs):
+        cr = func(*args, **kwargs)
+        next(cr)
+        return cr
+    return start
+
+
+@coroutine
+def split(*tc):
+    """
+    Coroutines to receive a value and send it to all coroutines specified
+    in ``tc`` list.
+
+    :Parameters:
+     tc
+        List of target coroutines.
+    """
+    while True:
+        v = yield
+        for c in tc:
+            c.send(v)
+
+
+# vim: sw=4:et:ai
