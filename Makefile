aae6dc18295776df3375394838ccf3ed9fa34476
wrobell <wrobell@pld-linux.org>
2013-08-27 20:13:46 +0100

    - documentation configuration added - makefile for documentation build and upload added

---
+++ Makefile
@@ -0,0 +1,15 @@
+RSYNC=rsync -zcav \
+	--exclude=\*~ --exclude=.\* \
+	--delete-excluded --delete-after \
+	--no-owner --no-group \
+	--progress --stats
+
+
+doc: .sphinx-stamp
+
+upload-doc:
+	$(RSYNC) build/doc/ wrobell@maszyna.it-zone.org:~/public_html/decotengu
+
+.sphinx-stamp:
+	sphinx-build-3 doc build/doc
+
---
+++ doc/conf.py
@@ -0,0 +1,27 @@
+import sys
+import os.path
+
+import decotengu
+
+sys.path.append(os.path.abspath('.'))
+sys.path.append(os.path.abspath('doc'))
+
+extensions = ['sphinx.ext.autodoc', 'sphinx.ext.doctest', 'sphinx.ext.todo',
+    'sphinx.ext.viewcode']
+project = 'decotengu'
+source_suffix = '.rst'
+master_doc = 'index'
+
+version = release = decotengu.__version__
+copyright = 'DecoTengu Team'
+
+epub_basename = 'decotengu - {}'.format(version)
+epub_author = 'DecoTengu Team'
+
+html_theme = 'agogo'
+html_theme_options = {
+    'headerbg': '#006bba',
+    'footerbg': '#006bba',
+}
+
+# vim: sw=4:et:ai
